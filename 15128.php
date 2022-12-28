5544259ec140803802149079d101b96f1628f406
jungmin0917 <cloonds@gmail.com>
2022-12-28 09:00:06 +0900

    15128번 (합동수) 풀이 완료

--- 15128.php
+++ 15128.php
@@ -7,8 +7,8 @@ $triangle = ($p1 / $q1) * ($p2 / $q2) * 0.5;
 // 부동소수점 때문에 엡실론을 사용한다 (컴퓨터 시스템 계산에서 가장 작은 차이라고 함)
 
 $val1 = $triangle;
-$val2 = floor($triangle);
-$epsilon = 0.00001;
+$val2 = round($triangle);
+$epsilon = 0.000001;
 
 if(abs($val1 - $val2) < $epsilon){
     $result = 1;
