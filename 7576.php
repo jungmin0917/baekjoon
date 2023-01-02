ac9339757c63fc3a1bbd08858b145ace99bf53d2
jungmin0917 <cloonds@gmail.com>
2023-01-03 00:02:11 +0900

    7576번 (토마토) 풀이 완료

--- 7576.php
+++ 7576.php
@@ -3,13 +3,15 @@
 // 풀이는 하단 링크 참고함
 // https://jae04099.tistory.com/entry/%EB%B0%B1%EC%A4%80-7576-%ED%86%A0%EB%A7%88%ED%86%A0-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%95%B4%EC%84%A4%ED%8F%AC%ED%95%A8
 
+// 기본 array 자료구조로는 한계가 있음. Spl을 이용해보도록 하겠음 (Standard PHP Library)
+
 fscanf(STDIN, "%d %d", $m, $n);
 
 // 최단거리 구하는 문제.
 // 최단거리는 bfs 즉, queue를 이용한다.
 // 모르겠으면 그림을 그려본다 (트리 모양으로)
 
-$tomato = array(); // 익은 토마토 좌표 (큐로 사용할 것임)
+$tomato = new SplQueue(); // 익은 토마토 좌표 (큐로 사용할 것임)
 
 // x, y축 변화량으로 방향 지정 (총 4방향)
 $dx = array(-1, 1, 0, 0);
@@ -24,7 +26,8 @@ for ($i=0; $i < $n; $i++) {
     for ($j=0; $j < $m; $j++) { 
         if($box[$i][$j] == 1){
             // 익어있는 토마토들의 최상단 가지(좌표)들을 큐에 넣자
-            array_unshift($tomato, array($j, $i)); // 0일부터의 day 정보
+            $tomato->unshift($j); // array로 넣을 시 메모리 초과하므로 따로따로 넣고 따로따로 뺌
+            $tomato->unshift($i);
         }
     }
 }
@@ -37,10 +40,13 @@ function more_less($a, $b, $c){ // 삼중 비교하는 함수
     }
 }
 
+$max = 0;
+
 // queue에서 더 이상 익힐 토마토가 없을 때까지 (새로 익힌 토마토를 큐에 넣으면서 진행)
-while(count($tomato) > 0){
+while(!($tomato->isEmpty())){
     // 맨 상단 트리부터 시작
-    list($x, $y) = array_pop($tomato);
+    $x = $tomato->pop(); // 큐에서 뺌
+    $y = $tomato->pop();
 
     for ($i=0; $i < 4; $i++) {
         $new_x = $x + $dx[$i];
@@ -51,8 +57,13 @@ while(count($tomato) > 0){
 
             // 새 토마토를 익히면서, 해당 위치를 현재 익힌 숫자의 +1을 함으로써 시작으로부터 몇 칸인지 적는다
             $box[$new_y][$new_x] = $box[$y][$x] + 1;
+            
+            if($box[$new_y][$new_x] > $max){
+                $max = $box[$new_y][$new_x];
+            }
 
-            array_unshift($tomato, array($new_x, $new_y)); // 큐에 넣음
+            $tomato->unshift($new_x);
+            $tomato->unshift($new_y);
         }
     }
 
@@ -60,24 +71,16 @@ while(count($tomato) > 0){
 
 $result = "";
 
-$max = 0;
-
 // 만약 0이 아직도 있으면 다 익히지 못함
 for ($i=0; $i < $n; $i++) { 
     if(in_array(0, $box[$i])){
         $result = -1;
         break;
     }
-
-    $now_max = max($box[$i]);
-
-    if($now_max > $max){
-        $max = $now_max;
-    }
 }
 
-if($result != '-1'){
-    $result = $max - 1; // 처음에 1로 시작했기 때문
+if($result != '-1'){ // 안 익은 토마토가 있는 경우
+	$result = ($max == 0) ? 0 : $max - 1;
 }
 
 echo $result;
