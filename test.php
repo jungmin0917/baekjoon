c708d3299f3358a04a6a5d3a57870e08fd6f0bdc
jungmin0917 <cloonds@gmail.com>
2023-02-23 23:00:38 +0900

    이것저것

--- test.php
+++ test.php
@@ -8,23 +8,8 @@ try{
 
 	$len = strlen($s);
 
-	$search = "2023";
-	$j = 0; // 찾는 index
 
-	$res = false;
-
-	for ($i=0; $i < $len; $i++) { 
-		if($s[$i] == $search[$j]){
-			$j++;
-		}
-
-		if($j == 4){
-			$res = true;
-			break;
-		}
-	}
-
-	if($res){
+	for ($i=0; $i < $len; $i++) {
 		
 	}
 
@@ -32,4 +17,20 @@ try{
 
 }catch(Exception $e){
 	echo $e->getMessage();
+}
+
+<?php
+function utf8_to_unicode($str) {
+    $code_point = 0;
+    $len = strlen($str);
+    $byte = 0;
+    for ($i = 0; $i < $len; $i++) {
+        $byte = ord($str[$i]);
+        if ($byte < 128 || $byte > 191) {
+            $code_point = ($code_point << 6) | ($byte & 0x7F);
+        } else {
+            $code_point = ($code_point << 6) | ($byte & 0x3F);
+        }
+    }
+    return $code_point;
 }
