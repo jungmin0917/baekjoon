82b04fb1069c1c48e3ab0903c1a04ce035aa3fe1
jungmin0917 <cloonds@gmail.com>
2023-02-23 23:10:57 +0900

    이것저것

--- test.php
+++ test.php
@@ -8,9 +8,13 @@ try{
 
 	$len = strlen($s);
 
+	$m = $len / 3;
 
-	for ($i=0; $i < $len; $i++) {
-		
+	for ($i=0; $i < $m; $i++) {
+		// 문자 하나의 유니코드 값을 먼저 출력
+		$unicode = utf8_to_unicode(substr($s, (3 * $i), (3 * ($i + 1))));
+
+		echo $unicode . "\n";
 	}
 
 	echo $result;
@@ -19,7 +23,6 @@ try{
 	echo $e->getMessage();
 }
 
-<?php
 function utf8_to_unicode($str) {
     $code_point = 0;
     $len = strlen($str);
--- 유용한 함수 - utf8 값을 unicode 값으로 반환.php
+++ 유용한 함수 - utf8 값을 unicode 값으로 반환.php
@@ -1,25 +1,22 @@
 <?php
 
+function ord8($c) {
+    $len = strlen($c);
+    if ($len <= 0) return false;
 
-// mb 모듈이 없으므로 UTF-8 인코딩 규칙에 따라 문자열을 분석하여 코드 포인트 값을 계산한다
-// UTF-8로 인코딩된 한글은 3바이트의 길이를 가진다
-// UTF-8에서는 첫 바이트의 상위 바이트가 1로 시작하면 해당 문자가 몇 바이트로 구성되어 있는지를 나타낸다
-// 한글은 3바이트로 구성되므로, 첫 바이트의 상위 비트는 1110이 된다
-// 첫번째 바이트에서 나머지 5비트를 추출한다
-// 두번째 바이트와 세번째 바이트에서 나머지 6비트를 추출한다
-// 뭔 소린지 잘 모르겠음
+    $h = ord($c[0]);
+    if ($h <= 0x7F) return $h; // 첫 번째 바이트가 0x7F 보다 작거나 같으면 한글이 아님
 
-function utf8_to_unicode($str) {
-    $code_point = 0;
-    $len = strlen($str);
-    $byte = 0;
-    for ($i = 0; $i < $len; $i++) {
-        $byte = ord($str[$i]);
-        if ($byte < 128 || $byte > 191) {
-            $code_point = ($code_point << 6) | ($byte & 0x7F);
-        } else {
-            $code_point = ($code_point << 6) | ($byte & 0x3F);
-        }
-    }
-    return $code_point;
+    // 첫 번째 바이트가 0xE0 이상 0xEF 이하인지 확인합니다.
+    if ($h < 0xE0 || $h > 0xEF) return false;
+
+    // 한글 문자의 두 번째와 세 번째 바이트를 가져옵니다.
+    $b2 = ord($c[1]);
+    $b3 = ord($c[2]);
+
+    // 두 번째와 세 번째 바이트가 0x80 이상 0xBF 이하인지 확인합니다.
+    if ($b2 < 0x80 || $b2 > 0xBF || $b3 < 0x80 || $b3 > 0xBF) return false;
+
+    // 한글 유니코드 값을 계산합니다.
+    return (($h & 0xF) << 12) | (($b2 & 0x3F) << 6) | ($b3 & 0x3F);
 }
